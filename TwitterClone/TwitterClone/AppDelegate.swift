5174b11e233b2ac8aa20eb50eb4ea68c7bb1cd45
tim-kim <timkim@jhu.edu>
2017-02-21 02:18:34 -0500

    Setup complete

---
+++ TwitterClone/TwitterClone/AppDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.swift
+//  TwitterClone
+//
+//  Created by Tim Kim on 2/21/17.
+//  Copyright © 2017 Tim Kim. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(url.description)
+        
+        let requestToken = BDBOAuth1Credential(queryString: url.query)
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com")!, consumerKey: "GlAZ62XdikrWIsdT25bYfKjuH", consumerSecret: "JAhMmEJ4A4nQF9XXsRcrS34OmRCM3IP4BQb5J7SpwykvnCHT2P")
+        
+        twitterClient?.fetchAccessToken(withPath: "oauth/access_token", method: "POST", requestToken: requestToken, success: { (accessToken: BDBOAuth1Credential?) -> Void in
+            print("I got the access token!")
+            
+            twitterClient?.get("1.1/account/verify_credentials.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
+                    print("account: \(response)")
+                }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
+                    //code
+                })
+            
+            }, failure: { (error: Error?) in
+                print("error: \(error?.localizedDescription)")
+            })
+        return true
+    }
+
+}
+
---
+++ TwitterClone/TwitterClone/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoginViewController.swift
+//  TwitterClone
+//
+//  Created by Tim Kim on 2/21/17.
+//  Copyright © 2017 Tim Kim. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    @IBAction func onLoginButton(_ sender: AnyObject) {
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com")!, consumerKey: "GlAZ62XdikrWIsdT25bYfKjuH", consumerSecret: "JAhMmEJ4A4nQF9XXsRcrS34OmRCM3IP4BQb5J7SpwykvnCHT2P")
+        
+        twitterClient?.deauthorize()
+        twitterClient?.fetchRequestToken(withPath: "oauth/request_token", method: "GET", callbackURL: URL(string: "twitterclone://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential?) -> Void in
+                print("request: \((requestToken?.token)!)")
+            
+            let url = URL(string: "https://api.twitter.com/oauth/authorize?=oauth_token=\((requestToken?.token)!)")!
+            UIApplication.shared.open(url)
+            
+        }, failure: { (error: Error?) in
+                print("error: \(error?.localizedDescription)")
+        })
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    
+}
